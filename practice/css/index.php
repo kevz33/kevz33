c0e684f99fe2e23bafbd9051f6e42c9ff1c87bfe
Kevin Castillo <kcastillo@csumb.edu>
2018-09-10 07:58:41 +0000

    added css precedence testing

---
+++ practice/css/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+ <link href="./sheet1.css" rel="stylesheet">
+</head>
+<body>
+    <style>
+        p{
+            margin: 10px;
+        }
+    </style>
+   <p class="highlighted">
+       
+       hfdaoibidohbhbhhbihdbfhfohdobfdiofhgohdoghfheoghoahobgeoohehohboahhgheuahyyyrgoahouihenhnnheohnoanhnhhanonhjKJJ
+   </p>
+   <p>
+       IPKHJGFRI09WHGUI9HJWG0JWI0RGJU8HIRHWIOGHIORHNGOHIOHHGIOEHIOGH
+   </p>
+    <p id="first">
+        ijhgieriwgijeiorhjighoehjiohjhjgpoej
+    </p>
+  <img src="https://scontent-frx5-1.cdninstagram.com/vp/b9dcafda7a9709cc6f2f1c89678f5a7f/5BFFC97C/t51.2885-15/sh0.08/e35/c0.135.1080.1080/s640x640/38713566_227817991219305_5835332311410802688_n.jpg"style="width:600px;">
+  <style>
+      img{
+          width: 20px;
+      }
+  </style>
+  <style>
+      h1{
+          width: 600px;
+         color:blue;
+         
+      }
+  </style>
+</body>
+
+</html>
