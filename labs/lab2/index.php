d4046893483ac4c238699230e1643463f948ed7e
Kevin Castillo <kcastillo@csumb.edu>
2018-09-19 17:23:36 +0000

    added

---
+++ labs/lab2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <div id="main">
+    <?php
+    include 'inc/functions.php';
+    ?>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+        <?php
+            play();
+        ?>
+        <form>
+            <input type= "submit" value="Spin!"/>
+        </form>
+        </div>
+        <style>
+            @import url("css/styles.css");
+        </style>
+        
+    </body>
+</html>
--- practice/php/intro.php
+++ practice/php/intro.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html>
     <?php
-        $firstname="Kevin";
-        echo"<div>$firstname</div>";
-        $fullname=$firstname."Castillo";
-        
-        ?>
-        
+        $i = 0;
+        $num = 0;
+        $sum = 0;
+        $avg = 0;
+        while ($i < 9)
+        {
+           $num = rand(0, 50);
+           $odd_or_even = ($num % 2 == 0)?"even":"odd";
+           echo $num ." ".$odd_or_even ."<br>";
+           
+           $sum += $num;
+           $i++;
+        }
+        echo "Sum = ".$sum."<br>";
+        $avg = $sum/9;
+        echo "Average = ".$avg;
+    ?>
 </html>
