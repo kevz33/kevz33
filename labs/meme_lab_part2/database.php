b9b22cf478e2ed474e39a2e86834a4b87fbe70dc
Kevin Castillo <kcastillo@csumb.edu>
2018-12-16 00:05:01 +0000

    meme lab part2

---
+++ labs/meme_lab_part2/database.php
@@ -0,0 +1,20 @@
+<?php
+// connect to our mysql database server
+function getDatabaseConnection() {
+    if (strpos($_SERVER['SERVER_NAME'], "c9users") !== false) {
+        // running on cloud9
+        $host = "localhost";
+        $username = "kevin";
+        $password = "cst300"; // best practice: define this in a separte file
+        $dbname = "memes_v2"; 
+    }
+    
+    
+    // Create connection
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConn; 
+}
+// temporary test code
+//$dbConn = getDatabaseConnection(); 
+?>
---
+++ labs/meme_lab_part2/navigation.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+      <li> <a href="index.php"> Home </a> </li>
+      <li> <a href="memes.php"> Memes </a> </li> 
+    </ul>
+    <div class="clear"></div>
+</nav>
